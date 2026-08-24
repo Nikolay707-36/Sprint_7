942ff70a226639873e6b4838c5c457c1452e7694
Nikolay707-36 <chursinov_ni@mail.ru>
2026-08-25 00:04:31 +0300

    Update test_orders_list.py

--- tests/test_orders_list.py
+++ tests/test_orders_list.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 from utils.helpers import BASE_URL
 
+
 class TestOrdersList:
     def test_orders_list_returns_list(self):
         response = requests.get(f"{BASE_URL}/orders")
